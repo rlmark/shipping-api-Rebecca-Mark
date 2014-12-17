f1faf091f322c8e5a339f95b92af90672615c70e
Rebecca Mark <rebecca.lmark@gmail.com>
2014-12-17 14:41:50 -0800

    working UPS query

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,16 +1,43 @@
+require 'active_shipping'
+include ActiveMerchant::Shipping
+
+# have a before action to make all things, in before action do teh render. Then no double render.
+# can assume that by the time it makes the quote, we can assume instance varibales are created.
+
+
 class QuotesController < ApplicationController
 
   def calculate
 
   # 1. check params for carrier to filter into appropriate API query
-    if params[:provider] == "usps"
-      usps_calculation
+    if params[:provider] == "fedex"
+      make_fedex_quote
+    elsif params[:provider] == "usps"
+      make_usps_quote
+    elsif params[:provider] == "ups"
+      make_ups_quote
     else
       render json: {error: "Must provide a postal carrier"}, status: :bad_request
     end
   end
 
-  def usps_calculation
+  # http://localhost:3000/quotes/calculate?provider=ups&origin_country=US&origin_state=CA&origin_city=Beverly+Hills&origin_zip=90210&destination_country=US&destination_state=WA&destination_city=Seattle&destination_zip=98105&package_weight=14
+  def make_ups_quote
+    ups = UPS.new(:login => ENV['UPS_LOGIN'], :password => ENV['UPS_KEY'], :key => ENV['UPS_KEY'])
+    response = ups.find_rates(make_origin, make_destination, make_package)
+    response_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    render json: response_rates
+  end
+
+  # http://localhost:3000/quotes/calculate?provider=fedex&origin_country=US&origin_state=CA&origin_city=Beverly+Hills&origin_zip=90210&destination_country=US&destination_state=WA&destination_city=Seattle&destination_zip=98105&package_weight=14
+  def make_fedex_quote
+    fedex = FedEx.new(test: true, :login => ENV['FEDEX_LOGIN'], :password => ENV['FEDEX_PASSWORD'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCOUNT'])
+    response = fedex.find_rates(make_origin, make_destination, make_package)
+    response_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    render json: response_rates
+  end
+
+  def make_usps_quote
     usps = USPS.new(:login => ENV["USPS_USERNAME"])
     response = usps.find_rates(make_origin, make_destination, make_package)
     response_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
@@ -38,7 +65,7 @@ class QuotesController < ApplicationController
 
   # http://localhost:3000/quotes/calculate?provider=usps&origin_country=US&origin_state=CA&origin_city=Beverly+Hills&origin_zip=90210&destination_country=US&destination_state=WA&destination_city=Seattle&destination_zip=98105&package_lbs=16
   def make_package
-    if params[:package_lbs].blank?
+    if !params[:package_lbs].blank?
       render json: {error: "Must provide a package weight"}, status: :bad_request
     else
       Package.new((params[:package_lbs].to_i * 16), [15, 10, 7], :cylinder => false)
